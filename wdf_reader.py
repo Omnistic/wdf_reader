35ef15119042ca7fc236789166eb8d2da4f706f0
David Nguyen <64271861+Omnistic@users.noreply.github.com>
2025-03-13 15:29:30 +0100

    Update wdf_reader.py

--- wdf_reader.py
+++ wdf_reader.py
@@ -1,11 +1,22 @@
+from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.lines as mlines
+from matplotlib.colors import Normalize
 import pprint
 import struct
+
+plt.style.use('dark_background')
 
 VALID_SIGNATURE = ['WDF1']
 SUPPORTED_VERSIONS = [0, 1]
 VALID_BLOCK_SIZE = [512]
+
+WDF_BLOCK_ID = {
+    'DATA': b'DATA',
+    'XLST': b'XLST',
+    'ORIGIN': b'ORGN'
+}
 
 SCAN_TYPES = [
     'Unspecified',
@@ -109,9 +120,11 @@ def read_wdf(file_path):
     
     wdf_header = get_header(wdf_bytes)
 
+    wdf_origin_list_info, wdf_origin_list = get_origin(wdf_bytes, wdf_header)
+
     wdf_spectra, wdf_x_axes, wdf_x_axes_labels = get_spectra(wdf_bytes, wdf_header)
 
-    return wdf_header, wdf_spectra, wdf_x_axes, wdf_x_axes_labels
+    return wdf_header, wdf_origin_list_info, wdf_origin_list, wdf_spectra, wdf_x_axes, wdf_x_axes_labels
 
 def get_header(wdf_bytes):
     wdf_header = {}
@@ -134,9 +147,67 @@ def get_header(wdf_bytes):
 
     return wdf_header
 
+def get_origin(wdf_bytes, wdf_header):
+    wdf_origin_list_info = []
+    wdf_origin_list = {}
+
+    origin_block_start_index = wdf_bytes.find(WDF_BLOCK_ID['ORIGIN']) + 20
+
+    origin_list_size = 24 + 8 * wdf_header['Capacity']
+
+    for ii in range(wdf_header['DataOriginCount']):
+        origin_data_index = origin_block_start_index + ii * origin_list_size
+        origin_data_type = struct.unpack('<I', wdf_bytes[origin_data_index:origin_data_index+4])[0]
+
+        origin_data_importance = 'Primary'
+
+        if origin_data_type >> 30 & 1:
+            origin_data_type = DATA_TYPES[-1]
+            origin_data_importance = 'Alternate'
+        else:
+            origin_data_type = DATA_TYPES[origin_data_type & 0x7FFFFFFF]
+
+        origin_data_unit = struct.unpack('<I', wdf_bytes[origin_data_index+4:origin_data_index+8])[0]
+        origin_data_unit = DATA_UNITS[origin_data_unit]
+
+        # I don't know what this is for. It resembles the origin data type, but it's not always the same.
+        # For example, when origin_data_type is 'Time', this strin gis 'Time'. But, when the origin_data_type
+        # is 'SpatialZ', this string becomes 'Z data'.
+        extra_info = struct.unpack('<16s', wdf_bytes[origin_data_index+8:origin_data_index+24])[0].decode('utf-8').rstrip('\x00')
+
+        wdf_origin_list_info.append({
+            'Type': origin_data_type,
+            'Unit': origin_data_unit,
+            'Importance': origin_data_importance,
+            'ExtraInfo': extra_info
+        })
+
+        origin_data_list = []
+
+        for jj in range(wdf_header['Capacity']):
+            origin_data_byte_start = origin_data_index + 24 + jj * 8
+            origin_data_byte = wdf_bytes[origin_data_byte_start:origin_data_byte_start+8]
+
+            match origin_data_type:
+                case 'SpatialZ':
+                    spatial_z = struct.unpack('<d', origin_data_byte)[0]
+
+                    origin_data_list.append(spatial_z)
+                case 'Time':
+                    timestamp = struct.unpack('<Q', origin_data_byte)[0]
+
+                    # Conversion Windows FILETIME to Unix timestamp
+                    timestamp = ( timestamp - 116444736000000000 ) / 10000000
+
+                    origin_data_list.append(datetime.fromtimestamp(timestamp))
+                    
+        wdf_origin_list[origin_data_type] = origin_data_list           
+
+    return wdf_origin_list_info, wdf_origin_list
+
 def get_spectra(wdf_bytes, wdf_header):
-    data_block_start_index = wdf_bytes.find(b'DATA')
-    xlst_block_start_index = wdf_bytes.find(b'XLST')
+    data_block_start_index = wdf_bytes.find(WDF_BLOCK_ID['DATA'])
+    xlst_block_start_index = wdf_bytes.find(WDF_BLOCK_ID['XLST'])
 
     x_axes = []
     x_axes_labels = []   
@@ -156,18 +227,58 @@ def get_spectra(wdf_bytes, wdf_header):
     return spectra, x_axes, x_axes_labels
 
 if __name__ == '__main__':
-    wdf_header, wdf_spectra, wdf_x_axes, wdf_x_axes_labels = read_wdf(r'\\scopem-staff.ethz.ch\\staff\\Nguyen.David\\work\\RAMAN\\epoxy_cure.wdf')
+    wdf_header, wdf_origin_list_info, wdf_origin_list, wdf_spectra, wdf_x_axes, wdf_x_axes_labels = read_wdf(r'\\scopem-staff.ethz.ch\\staff\\Nguyen.David\\work\\RAMAN\\epoxy_cure.wdf')
 
     pprint.pp(wdf_header)
+    pprint.pp(wdf_origin_list_info)
     pprint.pp(wdf_x_axes_labels[0])
 
-    plt.figure()
+    timepoints = []
 
-    for ii in range(len(wdf_spectra)):
-        plt.plot(wdf_x_axes[ii], wdf_spectra[ii])
+    start_time = wdf_origin_list['Time'][0]
 
-    plt.xlabel('{} ({})'.format(wdf_x_axes_labels[0]['XListType'], wdf_x_axes_labels[0]['XListUnits']))
-    plt.ylabel('({})'.format(wdf_header['SpectraUnit']))
-    plt.grid()
+    for ii in range(wdf_header['Count']):
+        dt = str(wdf_origin_list['Time'][ii] - start_time)
+        timepoints.append(dt[2:7])
 
+
+    # Create a figure with 2 subplots arranged horizontally 
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+    # Create a colormap for time progression
+    cmap = plt.cm.viridis  # You can also use 'plasma', 'inferno', 'magma', etc.
+    norm = Normalize(vmin=0, vmax=wdf_header['Count']-1)
+
+    # First subplot - LiveTrack Z Data with color gradient
+    for ii in range(wdf_header['Count']):
+        color = cmap(norm(ii))
+        ax1.plot(timepoints[ii], wdf_origin_list['SpatialZ'][ii], 'o', color=color)
+
+    ax1.set_title('LiveTrack Z Data vs Time')
+    ax1.set_xlabel('Time (mm:ss)')
+    ax1.set_ylabel('LiveTrack Z Data (um)')
+    ax1.grid()
+
+    # Second subplot - Spectra with color gradient and legend
+    legend_handles = []
+    for ii in range(wdf_header['Count']):
+        color = cmap(norm(ii))
+        line, = ax2.plot(wdf_x_axes[ii], wdf_spectra[ii], color=color)
+        # Create legend entry with the timepoint value
+        legend_handles.append(mlines.Line2D([], [], color=color, 
+                                        label=f'Time {timepoints[ii]}'))
+
+    ax2.set_xlabel('{} ({})'.format(wdf_x_axes_labels[0]['XListType'], wdf_x_axes_labels[0]['XListUnits']))
+    ax2.set_ylabel('({})'.format(wdf_header['SpectraUnit']))
+    ax2.set_xlim([2500, 2650])
+    ax2.set_ylim([0, 4000])
+    ax2.grid()
+
+    # Add the legend to the second subplot
+    ax2.legend(handles=legend_handles, title='Timepoints', loc='best')
+
+    # Adjust the spacing between subplots
+    plt.tight_layout()
+
+    # Show the figure
     plt.show()
